1d79e37591fd0b978aa968904d79ef447e3cc6d6
mcs232485 <141486520+mcs232485@users.noreply.github.com>
2023-09-18 20:33:43 +0530

    Add files via upload

--- SACHIN/New folder/run.sh
+++ SACHIN/New folder/run.sh
@@ -8,19 +8,20 @@ time=60
 bwnet=10
 # TODO: If you want the RTT to be 4ms what should the delay on each
 # link be?  Set this value correctly.
-delay=1.5
+delay=1
 
 iperf_port=5001
 
 modprobe tcp_probe
 # make sure we don't use a cached cwnd
 sysctl -w net.ipv4.tcp_no_metrics_save=1
-
+# make sure the local socket buffers don't become the bottleneck
+sysctl -w "net.ipv4.tcp_mem=10240 87380 268435456"
 
 for qsize in 20 100; do
     dir=bb-q$qsize
 
-    #mn -c > /dev/null 2>&1
+    mn -c > /dev/null 2>&1
 
     python bufferbloat.py --dir=$dir --time=$time --bw-net=$bwnet --delay=$delay --maxq=$qsize
 
@@ -30,9 +31,6 @@ for qsize in 20 100; do
     python plot_tcpprobe.py -f $dir/cwnd.txt -o $dir/cwnd-iperf.png -p $iperf_port
     python plot_queue.py -f $dir/q.txt -o $dir/q.png
     python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
-    cp $dir/cwnd-iperf.png ./cwind-q$qsize.png
-    cp $dir/q.png ./buffer-q$qsize.png
-    cp $dir/rtt.png ./rtt-q$qsize.png
 done
 
 echo "Simulations are done! Results can be retrieved via the server"
